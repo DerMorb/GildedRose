807b59b39f5e9f7e24de005852795a3c99b584a4
Michael Merk <Michael.Merk@inovex.de>
2019-07-16 15:21:26 +0200

    -added assertJ -added basic test (made the original more testable)

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,42 @@
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void testTheTruth() {
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+
+        assertThat(items.get(0).getSellIn()).isEqualTo(9);
+        assertThat(items.get(0).getQuality()).isEqualTo(19);
+
+        assertThat(items.get(1).getSellIn()).isEqualTo(1);
+        assertThat(items.get(1).getQuality()).isEqualTo(1);
+
+        assertThat(items.get(2).getSellIn()).isEqualTo(4);
+        assertThat(items.get(2).getQuality()).isEqualTo(6);
+
+        assertThat(items.get(3).getSellIn()).isEqualTo(0);
+        assertThat(items.get(3).getQuality()).isEqualTo(80);
+
+        assertThat(items.get(4).getSellIn()).isEqualTo(14);
+        assertThat(items.get(4).getQuality()).isEqualTo(21);
+
+        assertThat(items.get(5).getSellIn()).isEqualTo(2);
+        assertThat(items.get(5).getQuality()).isEqualTo(5);
+    }
 }
